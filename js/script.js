f84b7581cb3c7165d58f91ac952c967a310e1189
Brittany Brassell <brittany.brassell@gmail.com>
2015-11-16 01:01:50 -0800

    commit before removing scrollme

--- js/script.js
+++ js/script.js
@@ -2,12 +2,53 @@ $(document).ready(function(){
 	console.log('hello world');
 	
 	//scrollmagic
-	var controller = new ScrollMagic.Controller();
+
+	// Init ScrollMagic Controller
+  	var scrollMagicController = new ScrollMagic();
+  	
+  	//animates "star wars" to fade out repeatedly
+  	var tween = TweenMax.fromTo('.animationTrigger', 4,
+        {
+            scale: 5,
+         	left: -400
+    	},
+        {
+         scale: 0,
+         left: 400,
+         repeat: -1, /* Aka infinite amount of repeats */
+         yoyo: false /* Make it go back and forth */
+        });
+  
+	  // Create the Scene and trigger when visible
+	  var scene = new ScrollScene({
+	    triggerElement: '.starwars',
+	    duration: 700 /* How many pixels to scroll / animate */
+	  	}).setTween(tween);
+  
+	  // Add debug indicators fixed on right side
+	 
+	//should fade crawl text at top of page
+	 //  var bg_tween = TweenMax.from('.letterCrawl', 3, {
+		//   console.log('tween2 found');
+		//   backgroundColor: 'red',
+		//   scale: 5
+		// });
+	 //  var bg_scene = new  ScrollScene({
+		//   triggerElement: '.letterCrawl',
+		//   duration: 800
+		// }).setTween(bg_tween);
+
+	  //adds tweens to controller
+		scrollMagicController.addScene([
+  			scene
+		]);
+  
+
 
 
 	//animate.css
-	$('.fade').addClass('animated fadeOut');
-    $('.starwars').addClass('animated zoomOut');
+	//$('.fade').addClass('animated fadeOut');
+    //$('.starwars').addClass('animated zoomOut');
 
     //lettering.js
 	$(".star").lettering();
