899cd39f4360923f68063884c42781479337c721
Brittany Brassell <brittany.brassell@gmail.com>
2015-11-15 23:03:47 -0800

    starwars font, some animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 	console.log('hello world');
+	
+	//scrollmagic
+	var controller = new ScrollMagic.Controller();
+
+
+	//animate.css
+	$('.fade').addClass('animated fadeOut');
+    $('.starwars').addClass('animated zoomOut');
+
+    //lettering.js
+	$(".star").lettering();
+	$(".wars").lettering();
 
 });
